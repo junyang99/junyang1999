456d9bed9edba5dab365778b51577b72f3d45547
junyang99 <87479326+junyang99@users.noreply.github.com>
2024-03-09 19:50:47 +0800

    commit

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,6 +2,7 @@ import { defineStackbitConfig } from '@stackbit/types';
 import { GitContentSource } from '@stackbit/cms-git';
 import { allModels } from './.stackbit/models';
 
+
 const config = defineStackbitConfig({
     stackbitVersion: '~0.6.0',
     ssgName: 'nextjs',
